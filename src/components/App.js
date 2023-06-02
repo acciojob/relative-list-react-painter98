19cc57473506aa2083e78983c0e28144f2d21944
painter98 <emailmepallavi@gmail.com>
2023-06-02 11:16:01 +0530

    change2

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from './list.js';
 
 const App = () => {
-  let list = ['Doremon','Barbie','Ken','Thumbelina','Elsa','Kristoff'];
+ // let list = ['Doremon','Barbie','Ken','Thumbelina','Elsa','Kristoff'];
+  let [list,setList] = useState([]);
+  let [name,setName] = useState('');
+
+  function changing(){
+    console.log(name);
+    setList([...list,name]);
+  }
+
   return (
     <div id="main">
                {/* Do not remove the main div */}
+        <input type="text" onChange={e=>setName(e.target.value)}/>
+        <button onClick={changing}>submit</button>
         <List list={list}/>
     </div>
   )
